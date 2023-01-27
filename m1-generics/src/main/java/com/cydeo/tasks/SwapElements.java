d8519755484c85f118e3aca81f9a6c1219469704
Almi's PC <almimeci@gmail.com>
2023-01-27 15:39:51 -0600

    Start

---
+++ m1-generics/src/main/java/com/cydeo/tasks/SwapElements.java
@@ -0,0 +1,31 @@
+package com.cydeo.tasks;
+
+import java.util.Arrays;
+
+public class SwapElements {
+
+    /* Write a generic method to exchange the positions of two different elements in array. */
+
+
+    public static void main(String[] args) {
+
+//                      0  1  2  3  4  <-- index #s
+        int[] array1 = {1, 2, 3, 4, 5};
+        swap(array1, 0, 2);
+        System.out.println(Arrays.toString(array1));
+//       ---> [3, 2, 1, 4, 5]
+
+
+    }
+
+    private static void swap(int [] arr1, int index1, int index2){
+
+        int temp = arr1[index1];
+        arr1[index1] = arr1[index2];
+        arr1[index2] = temp;
+
+
+
+    }
+
+}
