eed4842a167b92c37eaa9f983d82d93b8fe1bdce
Almi's PC <almimeci@gmail.com>
2023-01-27 23:30:09 -0600

    11:30

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -5,11 +5,21 @@ public class ArraySorting {
     public static void main(String[] args) {
 
 
+        ArraySorting as = new ArraySorting();
+        as.sort();
 
+        QuickSort qs = new QuickSort();
+        qs.sort();
 
+        BubbleSort bs = new BubbleSort();
+        bs.sort();
 
 
     }
 
+
+    private void sort(){
+        System.out.println("Sorting Array");
+    }
 
 }
