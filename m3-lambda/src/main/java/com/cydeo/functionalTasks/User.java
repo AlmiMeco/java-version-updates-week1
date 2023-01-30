48ac631f58a7de03964f256aa3d7e8ff8c2d967f
Almi's PC <almimeci@gmail.com>
2023-01-29 20:35:11 -0600

    Finishes Day 45

---
+++ m3-lambda/src/main/java/com/cydeo/functionalTasks/User.java
@@ -0,0 +1,23 @@
+package com.cydeo.functionalTasks;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Data
+@NoArgsConstructor
+
+
+public class User {
+
+    private String firstName, lastName;
+    private int age;
+
+
+
+
+
+}
---
+++ m3-lambda/src/main/java/com/cydeo/functionalTasks/UserTest.java
@@ -0,0 +1,42 @@
+package com.cydeo.functionalTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class UserTest {
+
+
+    public static void main(String[] args) {
+
+
+        List <User> users = new ArrayList<>();
+        users.add(new User("Almi", "Meci", 21));
+        users.add(new User("Jack", "Black", 31));
+        users.add(new User("Koko", "Matter", 9));
+
+//        Print All Elements
+
+        printName(users, user -> true);
+
+
+
+//        Print All People that lastName starts with M
+
+        printName(users, user -> user.getLastName().startsWith("M"));
+
+    }
+
+    private static void printName(List<User> users, Predicate<User> p){
+
+        for (User user : users) {
+
+            if (p.test(user)){
+                System.out.println(user.toString());
+            }
+        }
+        
+    }
+
+
+}
