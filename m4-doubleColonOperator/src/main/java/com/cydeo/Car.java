a94d4c6448b7431aa477fcc6eeff6ec5e80e1ca4
Almi's PC <almimeci@gmail.com>
2023-01-30 19:58:51 -0600

    Day 46 {m4 finished @19:58}

---
+++ m4-doubleColonOperator/src/main/java/com/cydeo/Car.java
@@ -0,0 +1,21 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
+
+public class Car {
+
+    private String make;
+    private int year;
+
+
+
+
+}
---
+++ m4-doubleColonOperator/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,57 @@
+package com.cydeo;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+//        Predicate <Integer> pred = (p) -> p % 2 == 0 ? true : false;
+
+//        Calling Lambda from Interface (description of the method)
+        Calculate multiplyNumWInterface = (x,y) -> System.out.println(x*y);
+
+//        Calling Lambda from (static) class 'Calculator' *must have matching implementation & return as functionalInterf.*
+//        (method name)
+        Calculate multiplyNumWMethodName = (x, y) -> Calculator.findMultiplication(x,y);
+
+//                                                  (Method Reference)
+        Calculate multiplyNumWMethodRefrence = Calculator::findMultiplication;
+
+        multiplyNumWMethodRefrence.calculate(10,2);
+//        --> 20
+
+//  --------------------------------------------------------------------------------------------------------------------
+//  --------------------------------------------------------------------------------------------------------------------
+//  --------------------------------------------------------------------------------------------------------------------
+
+        Calculator calculator1 = new Calculator();
+
+        Calculate instanceMethodRefrence = calculator1::findAddition;
+        instanceMethodRefrence.calculate(12,2);
+
+
+        BiFunction<String, Integer, String> str = String::substring;
+        System.out.println(str.apply("BRASS", 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
