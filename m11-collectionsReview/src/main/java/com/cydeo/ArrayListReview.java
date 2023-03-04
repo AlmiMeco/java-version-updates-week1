02d65fb5742655ff9849fd476689775289e65470
Almi's PC <almimeci@gmail.com>
2023-03-04 16:59:15 -0600

    DAY 02: Collections Review Begins

---
+++ m11-collectionsReview/src/main/java/com/cydeo/ArrayListReview.java
@@ -0,0 +1,37 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListReview {
+
+    public static void main(String[] args) {
+
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(21, "Fark"));
+        students.add(new Student(452, "Jacque"));
+        students.add(new Student(3452, "Almi"));
+        students.add(new Student(215, "Toni"));
+
+
+
+        System.out.println("Printing w/ Legacy For-Loop......................");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
+
+
+        System.out.println("Printing w/ Iterator......................");
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ m11-collectionsReview/src/main/java/com/cydeo/Student.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+
+
+}
