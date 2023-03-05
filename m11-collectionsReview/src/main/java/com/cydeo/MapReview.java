99f47629948d7a687658d3b1aa37fe41fc92a04b
Almi's PC <almimeci@gmail.com>
2023-03-04 19:34:23 -0600

    TwoSum Optimal Solution

---
+++ m11-collectionsReview/src/main/java/com/cydeo/MapReview.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+    public static void main(String[] args) {
+
+
+        Map<Integer, String> studentMap = new HashMap<>();
+
+        studentMap.put(1, "Almi");
+        studentMap.put(2, "Arjan");
+        studentMap.put(3, "Lytfije");
+
+
+    }
+
+}
---
+++ m11-collectionsReview/src/main/java/com/cydeo/TwoSumOptimalSolution.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumOptimalSolution {
+
+
+//    [ O(n) ]
+    public static int[] twoSumOptimal(int[] array, final int target){
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            int potentialMatch = target - array[i];
+
+            if (map.containsKey(potentialMatch)) return new int[]{i, map.get(potentialMatch)};
+            else map.put(array[i], i);
+        }
+
+        return new int[]{};
+    }
+
+
+}
