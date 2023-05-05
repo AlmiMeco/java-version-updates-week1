be2a36ca4289b645b7983c9e6445a57e1d6bb14c
Almi's PC <almimeci@gmail.com>
2023-05-05 16:30:58 -0500

    DSA Day 07: TreeVisualizer added (insert method works!!)

---
+++ m16-dataStructures-Trees/src/main/java/TreeApp.java
@@ -0,0 +1,18 @@
+public class TreeApp {
+
+    public static void main(String[] args) {
+
+
+        MyTree tree = new MyTree();
+        int[] numbers = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+
+        for (int i = 0; i < numbers.length; i++) {
+            tree.insert(numbers[i]);
+        }
+
+        VisualizeTree.printTree(tree.root, null, false);
+
+
+    }
+
+}
